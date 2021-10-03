aae7b69bf4e0fe57b6477a683dcfbfdffa0a1367
Daria <Darya.Serber@all.me>
2021-10-03 13:35:56 +0300

    start empty  proj

---
+++ HomeworkSerber/HomeworkSerberApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeworkSerberApp.swift
+//  HomeworkSerber
+//
+//  Created by allme on 03.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct HomeworkSerberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
